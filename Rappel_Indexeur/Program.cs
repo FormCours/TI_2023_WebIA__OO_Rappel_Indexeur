a127c8ff3ad8201dee9f9f1cb6c1a7f8d4e68987
studentdot0500 <studentdot0500@TFNSDOT0500A>
2023-10-25 17:02:24 +0200

    Ajoutez des fichiers projet.

---
+++ Rappel_Indexeur/Program.cs
@@ -0,0 +1,48 @@
+﻿
+ExempleIndexeur ei = new ExempleIndexeur();
+
+string r1 = ei["mj"];
+string r2 = ei["pnj"];
+Console.WriteLine($"{r1}, {r2}");
+
+class ExempleIndexeur
+{
+    private string player = "Pauline";
+    private string pnj1 = "Julie";
+    private string pnj2 = "Maud";
+    private string mj = "Alexis";
+
+    public string this[string clef]
+    {
+        get
+        {
+            switch (clef.ToLower())
+            {
+                case "joueur":
+                    return player;
+                case "mj":
+                    return mj;
+                case "pnj":
+                    return (new Random()).Next(2) == 1 ? pnj1 : pnj2;
+                default:
+                    return "Un loup";
+            }
+        }
+    }
+
+
+    //public string get(string clef)
+    //{
+    //    switch (clef.ToLower())
+    //    {
+    //        case "joueur":
+    //            return player;
+    //        case "mj":
+    //            return mj;
+    //        case "pnj":
+    //            return (new Random()).Next(2) == 1 ? pnj1 : pnj2;
+    //        default:
+    //            return "Un loup";
+    //    }
+    //}
+}
